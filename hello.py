3347c26a03378fde4ff84b3ee959178a024a60e1
satyen550 <satyen550@gmail.com>
2023-08-17 20:15:28 +0530

    Added flash message and name text box

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Sample Secret'
+
+class NamerForm(FlaskForm):
+    name = StringField("Enter Your Name : ", validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
@@ -20,3 +28,19 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash(f"Hi {name} Form submitted Successfully!!")
+    return render_template('name.html',
+                           name = name,
+                           form = form)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
